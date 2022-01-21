cac3e6d4c6a23d1b8f4a672d35cfbe635beef68d
Oleksandr Veretennykov <alc.platon@gmail.com>
2022-01-21 21:52:13 +0100

    Actualizado

--- Personas/App.xaml.cs
+++ Personas/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Personas
 {
@@ -13,5 +7,10 @@ namespace Personas
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(
+                "NTY2OTI3QDMxMzkyZTM0MmUzMG85Q0toWFI2U2dGcVIwdjlXRHBDVGlHanpxZFlWVEJLcnZWUWtuSE5EekU9");
+        }
     }
 }
